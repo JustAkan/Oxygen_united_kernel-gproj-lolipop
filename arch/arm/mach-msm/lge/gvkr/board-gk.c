0be2df3cd57a0406177a5ad66540eb7c76d3d192
jollaman999 <admin@jollaman999.com>
2015-04-19 14:32:25 +0900

    thermal: Add Dynamic thermal control ;)
    When user changed cpu max clock, the max clock is restored by msm_thermal settings.
    
    This patch fix this issue and control thermal dynamically.
    
    When user changed the cpu max clock, msm_thermal settings will be automatically change.
    The clock settings in msm_thermal, will be set based on changed cpu max clock.
    And thermal settings in msm_thermal, will be optimize based on current cpu clock settings.
    
    This function enabled by default.
    You can turn off this function by typing,
    echo 0 > /sys/kernel/msm_thermal/conf/dynamic_thermal_control
    Or re-enable by typing,
    echo 1 > /sys/kernel/msm_thermal/conf/dynamic_thermal_control
    
    If dynamic_thermal_control set to 1, you can't adjust msm_thermal settings. It will be restored automatically by this function.
    If you want to adjust msm_thermal settings manuaaly, turn off this function. And use your device with caution.
    
    If you do not want whole of thermal control functions, just turn off thermal throttling in the kernel control app. (Not recommended!)
    
    Conflicts:
    	drivers/thermal/Kconfig
    	drivers/thermal/Makefile
    	include/linux/msm_thermal.h

--- arch/arm/mach-msm/lge/gvkr/board-gk.c
+++ arch/arm/mach-msm/lge/gvkr/board-gk.c
@@ -2330,6 +2330,25 @@ static struct platform_device msm_tsens_device = {
 	.id = -1,
 };
 
+#ifdef CONFIG_DYNAMIC_THERMAL_CONTROL  
+static struct msm_thermal_data msm_thermal_pdata = {  
+	.sensor_id = 7, 
+	.poll_ms = 1200,
+	.shutdown_temp = 90,
+
+	.allowed_max_high = 68,
+	.allowed_max_low = 56,
+	.allowed_max_freq = 918000,
+
+	.allowed_mid_high = 58,
+	.allowed_mid_low = 54,
+	.allowed_mid_freq = 1026000,
+
+	.allowed_low_high = 56,
+	.allowed_low_low = 50,
+	.allowed_low_freq = 1134000,
+ };
+#else /* CONFIG_DYNAMIC_THERMAL_CONTROL */
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 7,	
 #ifdef CONFIG_LGE_PM
@@ -2344,12 +2363,6 @@ static struct msm_thermal_data msm_thermal_pdata = {
 #endif
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
-#ifdef CONFIG_INTELLI_THERMAL
-  .freq_control_mask = 0xf,
-  .core_limit_temp_degC = 80,
-  .core_temp_hysteresis_degC = 10,
-  .core_control_mask = 0xe,
-#endif
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
